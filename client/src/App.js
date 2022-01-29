d3f833a2f82192ea3e50ac152a7cfc71eb00a365
nickdbmiller <nickbarrettmiller@gmail.com>
2022-01-29 16:26:57 -0500

    Add homepage route.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+
+import Home from "./components/Home";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
