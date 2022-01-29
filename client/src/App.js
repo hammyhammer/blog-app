7ff19c13ae8953b30511ad38d5fefcbf13489433
nickdbmiller <nickbarrettmiller@gmail.com>
2022-01-29 16:37:54 -0500

    Add routes to post list and create post form.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
 
 import Home from "./components/Home";
+import PostList from "./components/PostList";
+import CreatePost from "./components/CreatePost";
 
 import './App.css';
 
@@ -9,6 +11,8 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/posts" element={<PostList />} />
+        <Route path="/create" element={<CreatePost />} />
       </Routes>
     </div>
   );
