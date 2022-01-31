2cc465fd5f1e7dde4c2b414101ffa738dbcdd3e0
nickdbmiller <nickbarrettmiller@gmail.com>
2022-01-31 13:30:28 -0500

    Add route to update page.

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from "./components/Home";
 import PostList from "./components/PostList";
 import CreatePost from "./components/CreatePost";
+import UpdatePost from './components/UpdatePost';
 
 import './App.css';
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/posts" element={<PostList />} />
         <Route path="/create" element={<CreatePost />} />
+        <Route path="/update/:id" element={<UpdatePost />} />
       </Routes>
     </div>
   );
