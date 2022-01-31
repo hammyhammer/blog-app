d2edbe2a8cfe3139b27e0333564923593a9954ae
Hampton Jacobs <hamptonjacobs@Hannahs-Air.fios-router.home>
2022-01-30 21:33:09 -0500

    small css

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,10 @@
 import Layout from "./Layout";
 
 export default function Home() {
-    return (
-        <Layout>
-            <h1>Welcome Home</h1>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <h1 className="main-header">Welcome Home</h1>
+
+    </Layout>
+  );
 };
