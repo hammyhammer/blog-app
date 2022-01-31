bfa7e32ef7b77efe49c336b5028d5d3930b586b0
nickdbmiller <nickbarrettmiller@gmail.com>
2022-01-31 13:32:06 -0500

    Make each post a link to update page. Pass props.

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import DeleteButton from "../components/DeleteButton.jsx"
-import { deletePost } from "../services/post.js";
 import { getPosts } from "../services/post.js"
 import Layout from "./Layout";
 
@@ -15,6 +15,7 @@ export default function PostList() {
     };
     fetchPosts();
   }, [toggle]);
+  
   const handleToggle = () => {
     setToggle(!toggle)
   }
@@ -25,18 +26,26 @@ export default function PostList() {
         {posts?.map((post) => {
 
           return (
-            <li
+            <Link
+              to = {`/update/${post._id}`}
               key={post._id}
-              className="cards"
+              _id={post._id}
+              title={post.title}
+              content={post.content}
             >
-              <div>
-                <h3>{post.title}</h3>
-                <h4>{post.content}</h4>
-                <DeleteButton handleToggle={handleToggle}
-                  _id={post._id}
-                />
-              </div>
-            </li>
+              <li
+                className="cards"
+              >
+                <div>
+                  <h3>{post.title}</h3>
+                  <h4>{post.content}</h4>
+                  <DeleteButton 
+                    handleToggle={handleToggle}
+                    _id={post._id}
+                  />
+                </div>
+              </li>
+            </Link>
           )
         })}
       </ul>
