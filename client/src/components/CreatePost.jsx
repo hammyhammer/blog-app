e33edc6061d8fa18a6ec080f785177f5f1b270b3
Hampton Jacobs <hamptonjacobs@Hannahs-Air.fios-router.home>
2022-01-31 09:29:19 -0500

    working on create post

--- client/src/components/CreatePost.jsx
+++ client/src/components/CreatePost.jsx
@@ -1,20 +1,44 @@
 import Layout from "./Layout";
 
+const default_input = {
+  title: "",
+  content: "",
+}
+
 export default function CreatePost() {
-    return (
-        <Layout>
-            <h2 className="create-title">Create Post Page</h2>
-            <form className= "create-form">
-                <label htmlFor="title">Title</label>
-                <input
-                    type="text"
-                />
-                <label htmlFor="content">Content</label>
-                <input
-                    type="text"
-                />
-                <button className="create-button">Create Post (Dummy)</button>
-            </form>
-        </Layout>
-    );
+
+  const [input, setInput] = useState(default_input)
+
+  const handleTextInput = (event) => {
+    const { id, value } = event.target;
+    setInput(prevInput => ({
+      ...prevInput,
+      [id]: value,
+    }))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const fields = input;
+    const res = api.post("/", { fields })
+    setInput(default_input)
+  }
+
+
+  return (
+    <Layout>
+      <h2 className="create-title">Create Post Page</h2>
+      <form className="create-form">
+        <label htmlFor="title">Title</label>
+        <input
+          type="text"
+        />
+        <label htmlFor="content">Content</label>
+        <input
+          type="text"
+        />
+        <button className="create-button">Create Post (Dummy)</button>
+      </form>
+    </Layout>
+  );
 };
